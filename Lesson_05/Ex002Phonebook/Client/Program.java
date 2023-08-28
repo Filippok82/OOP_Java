14785cc8b26b772a8da5885378765709112d66e9
Irina Filippova <irina.filippova.mail@yandex.ru>
2023-08-28 11:55:15 +0300

    Lesson 05

---
+++ Lesson_05/Ex002Phonebook/Client/Program.java
@@ -0,0 +1,13 @@
+package Lesson_05.Ex002Phonebook.Client;
+
+
+import Lesson_05.Ex002Phonebook.UI.App;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        App.ButtonClick();
+     
+    }
+}
---
+++ Lesson_05/Ex003Math/Mathematics/Shapes/Triangle.java
@@ -0,0 +1,5 @@
+package Lesson_05.Ex003Math.Mathematics.Shapes;
+
+public class Triangle {
+    /// ???...
+}
